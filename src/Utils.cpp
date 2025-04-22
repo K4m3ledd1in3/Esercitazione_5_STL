0db0de94bf346105a1788f6198a6aa28065883e8
Kameleddine <s309439@studenti.polito.com>
2025-04-22 20:58:49 +0200

    modifica

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -8,7 +8,6 @@ namespace PolygonalLibrary
 {
 bool ImportMesh(PolygonalMesh& mesh)
 {
-
     if(!ImportCell0Ds(mesh))
         return false;
 
@@ -17,7 +16,6 @@ bool ImportMesh(PolygonalMesh& mesh)
 
     if(!ImportCell2Ds(mesh))
         return false;
-
     return true;
 }
 
@@ -36,9 +34,7 @@ bool ImportCell0Ds(PolygonalMesh& mesh)
         listLines.push_back(line);
 
     file.close();
-
     listLines.pop_front();
-
     mesh.NumCell0Ds = listLines.size();
     if (mesh.NumCell0Ds == 0)
     {
@@ -63,7 +59,7 @@ bool ImportCell0Ds(PolygonalMesh& mesh)
     	x = mesh.Cell0DsCoordinates(0, id);
     	y= mesh.Cell0DsCoordinates(1, id);
         mesh.Cell0DsId.push_back(id);
-        if(marker!=0 &&(eps < x && x < 1-eps ) && (eps < y && y < 1-eps) )
+        if( marker!=0 && ( eps < x && x < 1-eps ) && (eps < y && y < 1-eps) )
 		{
         	cout << "The cell " << id << " is as the cell that hasn't the marker stored correctly." << endl;
         	flag_marker = false;
@@ -112,12 +108,8 @@ bool ImportCell1Ds(PolygonalMesh& mesh)
         converter >>  id >> separator 
 		>> marker >> separator >> mesh.Cell1DsExtrema(0, id) >>
 		separator >>  mesh.Cell1DsExtrema(1, id);
-        mesh.Cell1DsId.push_back(id);
-        
-    }
-
-    
-    
+        mesh.Cell1DsId.push_back(id);  
+    } 
     return true;
 }
  
@@ -140,9 +132,7 @@ void NonZeroLength(PolygonalMesh& mesh)
 	}
 	if(flag)
 		cout << "No cell has zero length." << endl;
-}
-
- 
+} 
 bool ImportCell2Ds(PolygonalMesh& mesh)
 {
 	char separator=';';
@@ -167,7 +157,6 @@ bool ImportCell2Ds(PolygonalMesh& mesh)
         cerr << "There is no cell 2D" << endl;
         return false;
     }
-
     mesh.Cell2DsId.reserve(mesh.NumCell2Ds);
     for (const string& line : listLines)
     {
@@ -186,14 +175,12 @@ bool ImportCell2Ds(PolygonalMesh& mesh)
 		
   		mesh.Cell2DsVertices[id].reserve(num_vertices);
 		vertices.reserve(num_vertices);  
-   
      for(unsigned int i = 0; i < num_vertices; i++)
 			{
             unsigned int v;
             converter >> separator >> v;
     		vertices.push_back(v); 
-			}
-			
+			}		
         converter >> separator >> num_edges;
         edges.reserve(num_edges);
         mesh.Cell2DsVertices[id].reserve(num_vertices);
@@ -209,7 +196,6 @@ bool ImportCell2Ds(PolygonalMesh& mesh)
     }
     return true;
 }
-
 void NonZero_Area(PolygonalMesh& mesh){
 	double tau = 2.2*10e-16*2.2*10e-16;
 	double TotArea = 0.0;
@@ -220,27 +206,26 @@ void NonZero_Area(PolygonalMesh& mesh){
 			Area = 0.0;
 			vector<double> buff;
 			double X,Y;
-			buff.reserve(2*mesh.Cell2DsEdges[c].size());
+			buff.reserve( 2*mesh.Cell2DsEdges[c].size() );
 			for(size_t j = 0; j<mesh.Cell2DsEdges[c].size(); j++){
 				unsigned int k = mesh.Cell2DsEdges[c][j];
 				unsigned int s = mesh.Cell1DsExtrema(0,k);
-				double x_s = mesh.Cell0DsCoordinates(0,s);
-				double y_s = mesh.Cell0DsCoordinates(1,s);
+				double x_s = mesh.Cell0DsCoordinates(0,s); //vertex_x_coordinate.
+				double y_s = mesh.Cell0DsCoordinates(1,s); //vertex_y_coordinate.
 				buff[2*j] = x_s;
 				buff[2*j+1] = y_s;
-				X+=(x_s)/(double)(mesh.Cell2DsEdges[c].size());
-				Y+=(y_s)/(double)(mesh.Cell2DsEdges[c].size());
+				X+=(x_s) / ( (double)(mesh.Cell2DsEdges[c].size()) );//barycenter X by arithmetic mean on uniform density. .
+				Y+=(y_s) / ( (double)(mesh.Cell2DsEdges[c].size()) );//barycenter by arithmentic mean on uniform density.
 				}
-		 			
 			for(size_t i = 0; i<mesh.Cell2DsEdges[c].size(); i++)
 			{
-				double x_i = buff[(2*i)%(mesh.Cell2DsEdges[c].size())];
-				double y_i1 = buff[(2*i+3)%(mesh.Cell2DsEdges[c].size())];
-				double x_i1 = buff[(2*i+2)%(mesh.Cell2DsEdges[c].size())]; 
-				double y_i = buff[(2*i+1)%(mesh.Cell2DsEdges[c].size())];
+				double x_i = buff[(2*i)%(2*mesh.Cell2DsEdges[c].size())]; //%mod(#buff) needs in order to slide from last vertex up to the first, 
+				double y_i1 = buff[(2*i+3)%(2*mesh.Cell2DsEdges[c].size())];
+				double x_i1 = buff[(2*i+2)%(2*mesh.Cell2DsEdges[c].size())]; 
+				double y_i = buff[(2*i+1)%(2*mesh.Cell2DsEdges[c].size())];
 				x_i-=X;
 				y_i-=Y;
-				x_i1-=X;
+				x_i1-=X; //Computation of the area from the barycenter frame, it wouldn't change anything 'cause the area doesn't affected by the reference frame.
 				y_i1-=Y;
 				Area+=(double) (x_i*y_i1-x_i1*y_i)*(0.5);				
 			}
@@ -250,41 +235,10 @@ void NonZero_Area(PolygonalMesh& mesh){
 				cout << "The polygon at cell " << c << " has zero area" << endl;
 				flag = false;
 			}
-		 
-			
 		}
 		if(flag)
 			cout << "None of the cell is such as that has null area."<<endl;
 		cout << "Tot_Area " << TotArea << endl; 
-
 }
-
 }
-	/*	for(auto& k: mesh.Cell2DsEdges[c]) 
-			{
-				unsigned int p = mesh.Cell1DsExtrema(1,k);
-				unsigned int s = mesh.Cell1DsExtrema(0,k);
-				double x_p = mesh.Cell0DsCoordinates(0,p);
-				double y_p = mesh.Cell0DsCoordinates(1,p);
-				buff.push_back(mesh.Cell0DsCoordinates(0,s));
-				buff.push_back(mesh.Cell0DsCoordinates(1,s));
-				buff.push_back(x_p);
-				buff.push_back(y_p);
-				for(size_t i = 0; i<mesh.Cell2DsEdges[c].size(); i++){
-						unsigned int j = mesh.Cell2DsEdges[c][i]; //restituisce arco
-						unsigned int w = mesh.Cell1DsExtrema(0,j); //estremo iniziale arco
-						double x_w = mesh.Cell0DsCoordinates(0,w);
-						double y_w = mesh.Cell0DsCoordinates(1,w);
-						if(abs(x_w-x_p)<2.2*10e-16 && abs(y_w-y_p)<2.2*10e-16){
-							unsigned int h = mesh.Cell1DsExtrema(1,j);
-							buff.push_back(x_w);
-							buff.push_back(y_w);
-							buff.push_back(mesh.Cell0DsCoordinates(0,h));
-							buff.push_back(mesh.Cell0DsCoordinates(1,h));
-							i = mesh.Cell2DsEdges[c].size();
-						}
-						
-				}
-				
-				
-			}*/
+	
